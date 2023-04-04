7519de5936f3ea2c14e8a0b0d8cb2791a5a9b7a3
Gordon Lay <gal65@uclive.ac.nz>
2023-04-04 18:53:38 +1200

    added RCC init

--- src/main.c
+++ src/main.c
@@ -1,56 +1,141 @@
 #include "stm32f4xx_hal.h"
 
-#define YELLOW_LED                             GPIO_PIN_13
-#define YELLOW_GPIO_PORT                       GPIOC
-#define C_GPIO_CLK_ENABLE()                    __HAL_RCC_GPIOC_CLK_ENABLE()
-#define RED_LED                                GPIO_PIN_14
-#define RED_GPIO_PORT                          GPIOC
-#define BLUE_LED                               GPIO_PIN_15
-#define BLUE_GPIO_PORT                         GPIOB
-#define B_GPIO_CLK_ENABLE()                    __HAL_RCC_GPIOB_CLK_ENABLE()
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+void Error_Handler(void);
 
-void LED_Init();
+int main(void)
+{
 
-int main(void) {
   HAL_Init();
-  LED_Init();
+  SystemClock_Config();
+  MX_GPIO_Init();
 
   while (1)
   {
-    HAL_GPIO_TogglePin(BLUE_GPIO_PORT, BLUE_LED);
-    HAL_GPIO_TogglePin(YELLOW_GPIO_PORT, YELLOW_LED);
-    HAL_GPIO_TogglePin(RED_GPIO_PORT, RED_LED);
+    HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_14);
     HAL_Delay(1000);
+    // HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_15);
+    // HAL_Delay(100);
+    // HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
+    // HAL_Delay(100);
+    // HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
+    // HAL_Delay(100);
+    // HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_14);
+    // HAL_Delay(100);
+    // HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_14);
+    // HAL_Delay(100);
+  }
+}
+
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 8;
+  RCC_OscInitStruct.PLL.PLLN = 72;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 4;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
   }
 
-  return 0;
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
 }
 
-void LED_Init() {
-  B_GPIO_CLK_ENABLE();
-  C_GPIO_CLK_ENABLE();
-  GPIO_InitTypeDef GPIO_InitStruct_BLUE;
-  GPIO_InitStruct_BLUE.Pin = BLUE_LED;
-  GPIO_InitStruct_BLUE.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct_BLUE.Pull = GPIO_PULLUP;
-  GPIO_InitStruct_BLUE.Speed = GPIO_SPEED_HIGH;
-  HAL_GPIO_Init(BLUE_GPIO_PORT, &GPIO_InitStruct_BLUE);
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
 
-  GPIO_InitTypeDef GPIO_InitStruct_YELLOW;
-  GPIO_InitStruct_YELLOW.Pin = YELLOW_LED;
-  GPIO_InitStruct_YELLOW.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct_YELLOW.Pull = GPIO_PULLUP;
-  GPIO_InitStruct_YELLOW.Speed = GPIO_SPEED_HIGH;
-  HAL_GPIO_Init(YELLOW_GPIO_PORT, &GPIO_InitStruct_YELLOW);
-  
-  GPIO_InitTypeDef GPIO_InitStruct_RED;
-  GPIO_InitStruct_RED.Pin = RED_LED;
-  GPIO_InitStruct_RED.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct_RED.Pull = GPIO_PULLUP;
-  GPIO_InitStruct_RED.Speed = GPIO_SPEED_HIGH;
-  HAL_GPIO_Init(RED_GPIO_PORT, &GPIO_InitStruct_RED);
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOH_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_RESET);
+
+  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_14, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_15, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : PC13 */
+  GPIO_InitStruct.Pin = GPIO_PIN_13;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+    /*Configure GPIO pin : PC14 */
+  GPIO_InitStruct.Pin = GPIO_PIN_14;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : PB15 */
+  GPIO_InitStruct.Pin = GPIO_PIN_15;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
 }
 
-void SysTick_Handler(void) {
+/* USER CODE BEGIN 4 */
+void SysTick_Handler(void) 
+{
   HAL_IncTick();
 }
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
